226f29bc4d2178cc5095b6786689a18a56a16b8a
samda08 <bma649212@naver.com>
2025-11-23 20:01:32 +0900

    data 모듈화

--- data/__init__.py
+++ data/__init__.py
@@ -1,13 +1,11 @@
+"""
+data/__init__.py
+데이터 모듈
+"""
+
 from .dataset import EnglishDialectsDataset, collate_fn
-from .preprocessing import AudioPreprocessor, SpecAugment
-from .data_config import REGION_LABELS, GENDER_LABELS, REGION_COORDS
 
 __all__ = [
     'EnglishDialectsDataset',
-    'collate_fn',
-    'AudioPreprocessor',
-    'SpecAugment',
-    'REGION_LABELS',
-    'GENDER_LABELS',
-    'REGION_COORDS'
+    'collate_fn'
 ]
